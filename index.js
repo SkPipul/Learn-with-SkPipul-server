0f45eee92414ccc7c8673cdeacb8ea6980d90a8e
SkPipul <skjabed252@gmail.com>
2022-10-25 20:03:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// const express = require('express')
+// const app = express();
+// const cors = require('cors')
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+
+// const categories = require('./data/category.json');
+
+// app.get('/', (req, res) => {
+//     res.send('Course is started YAY!!');
+
+// })
+
+// app.get('/course-categories', (req, res) => {
+//     res.send(categories)
+// })
+
+// app.listen(port, () => {
+//     console.log("learn with skpipul server is running on port", port);
+// })
