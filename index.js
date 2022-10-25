39d8629e694ced7b295ac357ee0bc86b75c56b93
SkPipul <skjabed252@gmail.com>
2022-10-25 22:16:41 +0600

    image added

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/category.json');
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('Course is started YAY!!');
 
-})
+});
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find( course => course.id === id);
+    res.send(course);
 })
 
 app.listen(port, () => {
