f2444c03d2b63a6bc9e6fde91fa3f7ce722819fc
SkPipul <skjabed252@gmail.com>
2022-10-26 13:35:40 +0600

    unnessesary code deleted

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/category.json');
 const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
@@ -13,15 +12,17 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/course-categories', (req, res) => {
-    res.send(categories)
-});
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const course = courses.find( course => course.id === id);
     res.send(course);
-})
+});
+
+
 
 app.listen(port, () => {
     console.log("learn with skpipul server is running on port", port);
